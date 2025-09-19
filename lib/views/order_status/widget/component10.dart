37f52b532518b6e33f9bea5f6bb98354c37e0b25
faisalsalamehd <faisal1991sam@gmail.com>
2025-09-19 12:14:07 +0300

    refactor: update padding and improve layout structure in Component10

--- lib/views/order_status/widget/component10.dart
+++ lib/views/order_status/widget/component10.dart
@@ -14,7 +14,7 @@ class Component10 extends StatelessWidget {
     return GetBuilder<OrderStatusController>(builder: (ctrl) {
       return Container(
         margin: const EdgeInsets.symmetric(vertical: 15),
-        padding: const EdgeInsets.all(24),
+        padding: const EdgeInsets.symmetric(vertical: 16),
         decoration: BoxDecoration(
           border: Border.all(
             color: const Color(0xffC3CCD3),
@@ -24,94 +24,105 @@ class Component10 extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                SvgPicture.asset(
-                  SvgAssets.summuryIcon,
-                  fit: BoxFit.cover, // يحافظ على نسب الصورة ويملأ الـ Container
-                  width: 21.5, // اختياري: عرض محدد
-                  height: 21.5,
-
-                  // اختياري: ارتفاع محدد
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                const Text(
-                  "ملخص الحجز",
-                  style: TextStyle(
-                    fontFamily: 'IBMPlexSansArabic',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                    height: 25 / 16,
-                    letterSpacing: 0,
-                    color: Color(0xff121212),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    SvgAssets.summuryIcon,
+                    fit: BoxFit.cover, // يحافظ على نسب الصورة ويملأ الـ Container
+                    width: 21.5, // اختياري: عرض محدد
+                    height: 21.5,
+              
+                    // اختياري: ارتفاع محدد
                   ),
-                  textAlign: TextAlign.start,
-                ),
-              ],
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 2,
-              height: 30,
-            ),
-            Text(
-              washDataTripModel.washTitleAr ?? "",
-              style: const TextStyle(
-                fontFamily: 'IBMPlexSansArabic',
-                fontWeight: FontWeight.w600,
-                fontSize: 14,
-                height: 25 / 14,
-                letterSpacing: 0,
-                color: Color(0xff121212),
-              ),
-              textAlign: TextAlign.start,
-            ),
-            SizedBox(
-              height: AppSize.width * 0.05,
-            ),
-            Text(
-              washDataTripModel.washTitleAr ?? "",
-              style: const TextStyle(
-                fontFamily: 'IBMPlexSansArabic',
-                fontWeight: FontWeight.w500,
-                fontSize: 14,
-                height: 32 / 16,
-                letterSpacing: 0,
-                color: Color(0xff29C1F2),
-              ),
-              textAlign: TextAlign.start,
-            ),
-            const Text("رقم السجل الضريبي : 14214689124",
-                style: TextStyle(
-                  fontFamily: 'IBMPlexSansArabic',
-                  fontWeight: FontWeight.w400,
-                  fontSize: 10,
-                  height: 32 / 10,
-                  letterSpacing: 0,
-                  color: Color(0xff8193B3),
-                ),
-                textAlign: TextAlign.start),
-            SizedBox(
-              height: AppSize.width * 0.05,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("الذهاب الى قائمة الفواتير",
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text(
+                    "ملخص الحجز",
                     style: TextStyle(
                       fontFamily: 'IBMPlexSansArabic',
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                      height: 25 / 18,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      height: 25 / 16,
                       letterSpacing: 0,
                       color: Color(0xff121212),
                     ),
-                    textAlign: TextAlign.start),
-                Icon(Icons.arrow_forward_ios)
-              ],
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
             ),
+             Divider(
+              color: Colors.grey.withValues(alpha: 0.3),
+              thickness: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    washDataTripModel.washTitleAr ?? "",
+                    style: const TextStyle(
+                      fontFamily: 'IBMPlexSansArabic',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      height: 25 / 14,
+                      letterSpacing: 0,
+                      color: Color(0xff121212),
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  SizedBox(
+                    height: AppSize.width * 0.05,
+                  ),
+                  Text(
+                    washDataTripModel.washTitleAr ?? "",
+                    style: const TextStyle(
+                      fontFamily: 'IBMPlexSansArabic',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      height: 32 / 16,
+                      letterSpacing: 0,
+                      color: Color(0xff29C1F2),
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  const Text("رقم السجل الضريبي : 14214689124",
+                      style: TextStyle(
+                        fontFamily: 'IBMPlexSansArabic',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 10,
+                        height: 32 / 10,
+                        letterSpacing: 0,
+                        color: Color(0xff8193B3),
+                      ),
+                      textAlign: TextAlign.start),
+                  SizedBox(
+                    height: AppSize.width * 0.05,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("الذهاب الى قائمة الفواتير",
+                          style: TextStyle(
+                            fontFamily: 'IBMPlexSansArabic',
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18,
+                            height: 25 / 18,
+                            letterSpacing: 0,
+                            color: Color(0xff121212),
+                          ),
+                          textAlign: TextAlign.start),
+                      Icon(Icons.arrow_forward_ios)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+         
           ],
         ),
       );
